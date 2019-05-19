9b77c9a4f51e691bf08d5ca21fd592f1cef5c60e
mccaffertycr <mccaffertycr@gmail.com>
2019-05-19 16:15:54 -0400

    finished implementing hook

--- client/src/Boxscore.js
+++ client/src/Boxscore.js
@@ -9,16 +9,17 @@ import {
 } from './styles';
 
 const Boxscore = ({ league }) => {
-  const { gameData, headerRowVals, isLoading } = useGameData(league);
-  if (!isLoading) {
-    const {
-      event_information,
-      away_team,
-      away_period_scores,
-      home_team,
-      home_period_scores,
-    } = gameData.data;
-    if (away_team) {
+  const { gameData, headerRowVals } = useGameData(league);
+
+  if (gameData) {
+    if (gameData.away_team) {
+      const {
+        event_information,
+        away_team,
+        away_period_scores,
+        home_team,
+        home_period_scores,
+      } = gameData;
       return (
         <BoxscoreContainer>
           {/* Header Row */}
@@ -40,18 +41,14 @@ const Boxscore = ({ league }) => {
             {away_period_scores.map((val, i) => (
               <BoxscoreCell key={i}>{val}</BoxscoreCell>
             ))}
-            {gameData.data.away_batter_totals ? (
+            {gameData.away_batter_totals ? (
               <>
-                <BoxscoreCell>
-                  {gameData.data.away_batter_totals.rbi}
-                </BoxscoreCell>
-                <BoxscoreCell>
-                  {gameData.data.away_batter_totals.hits}
-                </BoxscoreCell>
-                <BoxscoreCell>{gameData.data.away_errors}</BoxscoreCell>
+                <BoxscoreCell>{gameData.away_batter_totals.rbi}</BoxscoreCell>
+                <BoxscoreCell>{gameData.away_batter_totals.hits}</BoxscoreCell>
+                <BoxscoreCell>{gameData.away_errors}</BoxscoreCell>
               </>
             ) : (
-              <BoxscoreCell>{gameData.data.away_totals.points}</BoxscoreCell>
+              <BoxscoreCell>{gameData.away_totals.points}</BoxscoreCell>
             )}
           </BoxscoreRow>
           {/* Home Team Row*/}
@@ -60,18 +57,14 @@ const Boxscore = ({ league }) => {
             {home_period_scores.map((val, i) => (
               <BoxscoreCell key={i}>{val}</BoxscoreCell>
             ))}
-            {gameData.data.away_batter_totals ? (
+            {gameData.away_batter_totals ? (
               <>
-                <BoxscoreCell>
-                  {gameData.data.home_batter_totals.rbi}
-                </BoxscoreCell>
-                <BoxscoreCell>
-                  {gameData.data.home_batter_totals.hits}
-                </BoxscoreCell>
-                <BoxscoreCell>{gameData.data.home_errors}</BoxscoreCell>
+                <BoxscoreCell>{gameData.home_batter_totals.rbi}</BoxscoreCell>
+                <BoxscoreCell>{gameData.home_batter_totals.hits}</BoxscoreCell>
+                <BoxscoreCell>{gameData.home_errors}</BoxscoreCell>
               </>
             ) : (
-              <BoxscoreCell>{gameData.data.home_totals.points}</BoxscoreCell>
+              <BoxscoreCell>{gameData.home_totals.points}</BoxscoreCell>
             )}
           </BoxscoreRow>
           <BoxscoreTeamInfoRow>
--- client/src/useGameData.js
+++ client/src/useGameData.js
@@ -1,16 +1,17 @@
 import React, { useState, useEffect } from 'react';
 
 const useGameData = league => {
-  const [gameData, setGameData] = useState({ data: {} });
+  const [gameData, setGameData] = useState(null);
+  const [headerRowVals, setHeaderRowVals] = useState([]);
+  const [initialRender, setInitialRender] = useState(true);
   const [isLoading, setIsLoading] = useState(false);
   const [isError, setIsError] = useState(false);
-  const [headerRowVals, setHeaderRowVals] = useState([]);
 
   useEffect(() => {
+    let mounted = true;
     const fetchData = async () => {
       setIsError(false);
       setIsLoading(true);
-
       try {
         const result = await fetch(`/api/games/${league}`);
         const data = await result.json();
@@ -18,17 +19,25 @@ const useGameData = league => {
         if (data.data.league === 'NBA') setHeaderRowVals([1, 2, 3, 4, 'T']);
         if (data.data.league === 'MLB')
           setHeaderRowVals([1, 2, 3, 4, 5, 6, 7, 8, 9, 'R', 'H', 'E']);
-        setGameData({ data: data.data });
+        if (mounted) setGameData(data.data);
       } catch (error) {
         setIsError(true);
       }
-
       setIsLoading(false);
     };
-    fetchData();
-    // let gameDataIntervalId = setInterval(() => fetchData(), 16000);
-    // return () => clearInterval(gameDataIntervalId);
-  }, [league]);
+
+    if (initialRender) {
+      fetchData();
+      setInitialRender(false);
+    } else {
+      let gameDataIntervalId = setInterval(() => fetchData(), 16000);
+      const cleanup = () => {
+        mounted = false;
+        clearInterval(gameDataIntervalId);
+      };
+      return cleanup;
+    }
+  }, [league, initialRender]);
 
   return { gameData, headerRowVals, isLoading, isError };
 };
